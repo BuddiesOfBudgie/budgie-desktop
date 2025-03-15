72e63687d13e96c260c5f6da5a271b1211ec4eed
Evan Maddock <5157277+EbonJaeger@users.noreply.github.com>
2025-03-15 13:13:07 -0400

    budgie-popover: Add missing NULL terminator to g_object_new (#684)
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/plugin/panel/popover.c
+++ src/plugin/panel/popover.c
@@ -12,13 +12,13 @@
 #include "popover.h"
 
 struct BudgiePopover {
-    GtkWidget * relative_to;
 };
 
 G_DEFINE_TYPE(BudgiePopover, budgie_popover, GTK_TYPE_POPOVER)
 
 static void budgie_popover_init(BudgiePopover * self){
-
+	GtkStyleContext * style = gtk_widget_get_style_context(self);
+	gtk_style_context_add_class(style, "budgie-popover");
 }
 
 static void budgie_popover_class_init(BudgiePopoverClass * c) {
@@ -26,9 +26,7 @@ static void budgie_popover_class_init(BudgiePopoverClass * c) {
 }
 
 GtkWidget * budgie_popover_new(GtkWidget * relative_to) {
-    GtkWidget * popover = g_object_new(BUDGIE_TYPE_POPOVER, "relative-to", relative_to);
-    GtkStyleContext * style = gtk_widget_get_style_context(popover);
-    gtk_style_context_add_class(style, "budgie-popover");
+	g_return_val_if_fail (relative_to == NULL || GTK_IS_WIDGET (relative_to), NULL);
 
-    return popover;
+	return g_object_new(BUDGIE_TYPE_POPOVER, "relative-to", relative_to, NULL);
 }
